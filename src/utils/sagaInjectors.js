cd55d552527348d35249f8812aa85f7e2f62f438
lichun <chunli@sf-express.com>
2018-11-02 12:06:34 +0800

    finish sagaInjectors

--- src/utils/sagaInjectors.js
+++ src/utils/sagaInjectors.js
@@ -22,7 +22,7 @@ const checkKey = key => invariant(
 const checkDescriptor = (descriptor) => {
   const shape = {
     saga: isFunction,
-    mode: (mode) => isString(mode) && allowedModes.includes(mode),
+    mode: mode => isString(mode) && allowedModes.includes(mode),
   };
 
   invariant(
@@ -36,26 +36,54 @@ export function injectSagaFactory(store, isValid) {
     if (!isValid) checkStore(store);
 
     const newDescriptor = { ...descriptor, mode: descriptor.mode || RESTART_ON_REMOUNT };
-  }
+    const { saga, mode } = newDescriptor;
+
+    checkKey(key);
+    checkDescriptor(newDescriptor);
+
+    let hasSaga = Reflect.has(store.injectedSagas, key);
+
+    if (process.env.NODE_ENV !== 'production') {
+      const oldDescriptor = store.injectedSagas[key];
+      if (hasSaga && oldDescriptor.saga !== saga) {
+        oldDescriptor.task.cancel();
+        hasSaga = false;
+      }
+    }
+
+    if (!hasSaga || (hasSaga && mode !== DAEMON && mode !== ONCE_TILL_UNMOUNT)) {
+      // eslint-disable-next-line
+      store.injectedSagas[key] = { ...newDescriptor, task: store.runSaga(saga, args) };
+    }
+  };
 }
 
+export function ejectSagaFactory(store, isValid) {
+  return function ejectSaga(key) {
+    if (!isValid) checkStore(store);
 
+    checkKey(key);
 
+    if (Reflect.has(store.injectedSagas, key)) {
+      const descriptor = store.injectedSagas[key];
 
-// function injectSaga(key, obj, props, store) {
-//   const { saga, mode } = obj;
-//   store.run(saga);
-// }
+      if (descriptor.mode !== DAEMON) {
+        descriptor.task.cancel();
 
-// function ejectSaga(key, store) {
+        if (process.env.NODE_ENV === 'production') {
+          // Need some value to be able to detect `ONCE_TILL_UNMOUNT` sagas in `injectSaga`
+          store.injectedSagas[key] = 'done'; // eslint-disable-line no-param-reassign
+        }
+      }
+    }
+  };
+}
 
-// }
+export default function getInjectors(store) {
+  checkStore(store);
 
-// function getInjectors(store) {
-//   return {
-//     injectSaga: (key, obj, props) => injectSaga(key, obj, props, store),
-//     ejectSaga: key => ejectSaga(key, store),
-//   };
-// }
-
-// export default getInjectors;
+  return {
+    injectSaga: injectSagaFactory(store, true),
+    ejectSaga: ejectSagaFactory(store, true),
+  };
+}
