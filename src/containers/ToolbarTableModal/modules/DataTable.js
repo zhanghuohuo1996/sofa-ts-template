7e20a64dfdce229ec690d812007fbfdf5de2db98
lichun <yxlichun@126.com>
2018-11-04 14:48:49 +0800

    tong table

--- src/containers/ToolbarTableModal/modules/DataTable.js
+++ src/containers/ToolbarTableModal/modules/DataTable.js
@@ -1,49 +1,60 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import {
-  Form,
-  Row,
-  Col,
-  Input,
+  Table,
 } from 'antd';
 
-// eslint-disable-next-line
-export default class Toolbar extends React.PureComponent {
-  state = {
-    values: {},
+import connectFactory from 'utils/connectFactory';
+
+import { NAMESPACE } from '../constants';
+import { postFormData } from '../actions';
+
+const withConnect = connectFactory(NAMESPACE);
+
+@withConnect(
+  state => ({
+    mainData: state.get('mainData').toJS(),
+  }),
+  {
+    postFormData,
+  },
+)
+class DataTable extends React.Component {
+  static propTypes = {
+    mainData: PropTypes.array.isRequired,
   };
 
-  handleChange(e, key) {
-    const { values } = this.state;
-    const { value } = e.target;
-    this.setState({
-      values: {
-        ...values,
-        [key]: value,
-      },
-    });
-  }
+  columns = [{
+    title: '姓名',
+    dataIndex: 'name',
+    key: 'name',
+  }, {
+    title: '年龄',
+    dataIndex: 'age',
+    key: 'age',
+  }];
 
   render() {
-    const { values } = this.state;
+    const { mainData } = this.props;
 
     return (
-      <Form>
-        <Row gutter={24}>
-          <Col span={6}>
-            <Form.Item label="姓名">
-              <Input
-                placeholder="请输入姓名"
-                onChange={e => this.handleChange(e, 'name')}
-                value={values.name}
-              ></Input>
-            </Form.Item>
-          </Col>
-          <Col span={6}>
-            <Form.Item label="年龄">
-              <Input placeholder="请输入年龄" onChange={e => this.handleChange(e, 'age')}></Input>
-            </Form.Item>
-          </Col>
-        </Row>
-      </Form>);
+      <Table
+        bordered
+        // loading={loading}
+        columns={this.columns}
+        dataSource={mainData}
+        rowKey="id"
+        pagination={{
+          // defaultCurrent: 1,
+          // current: pagination.page,
+          // total: pagination.total,
+          // showTotal: this.showTotal,
+          // pageSize: pagination.perpage,
+          // onChange: this.handlePageChange,
+        }}
+      />
+    );
   }
 }
+
+export default DataTable;
--- src/containers/ToolbarTableModal/reducer.js
+++ src/containers/ToolbarTableModal/reducer.js
@@ -5,6 +5,23 @@ const initialState = fromJS({
     name: 'lichun',
     age: '',
   },
+  mainData: [{
+    id: 1,
+    name: '李淳',
+    age: 18,
+  }, {
+    id: 2,
+    name: '凤凤',
+    age: 88,
+  }, {
+    id: 3,
+    name: '天晴',
+    age: 99,
+  }, {
+    id: 4,
+    name: '肚肚疼',
+    age: 10,
+  }],
 });
 
 function reducer(state = initialState, action) {
