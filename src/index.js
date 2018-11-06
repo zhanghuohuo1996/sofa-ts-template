92ff1a690cb74fd7535adbff760c830ffc192510
ylq <458909861@qq.com>
2018-11-06 11:06:58 +0800

    国际化1.0

--- src/index.js
+++ src/index.js
@@ -3,49 +3,75 @@ import 'babel-polyfill';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import { IntlProvider, addLocaleData } from 'react-intl';
 
 import Main from 'containers/Main';
+import intl from 'intl';
+import zh from 'react-intl/locale-data/zh'; // react-intl语言包
+import en from 'react-intl/locale-data/en'; // react-intl语言包
+import { translationMessages } from './i18n';
+
+import zhTranslationMessages from './translations/zh';
+import enTranslationMessages from './translations/en';
+
 import storeFactory from './state/storeFactory';
 
 import './global-styles';
 import './theme/style.less';
-// Import i18n messages
-// import { translationMessages } from './i18n';
 
+addLocaleData([...en, ...zh]);
+const rootElement = document.getElementById('root');
 const initialState = {};
 const store = storeFactory(initialState);
 
-function App() {
-  return (
-    <Provider store={store}>
-      <Main />
-    </Provider>
-  );
+function chooseLocale() {
+  switch (navigator.language.split('-')[0]) {
+    case 'en':
+      return enTranslationMessages;
+      break;
+    case 'zh':
+      return zhTranslationMessages;
+      break;
+    default:
+      return enTranslationMessages;
+      break;
+  }
 }
 
-const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+const render = () => {
+  ReactDOM.render(
+    <Provider store={store}>
+      <IntlProvider
+        locale={navigator.language}
+        messages={chooseLocale()}
+      >
+        <Main />
+      </IntlProvider>
+    </Provider>,
+    rootElement,
+  );
+};
 
 if (module.hot) {
   module.hot.accept(['./i18n', 'containers/Main'], () => {
     ReactDOM.unmountComponentAtNode(rootElement);
-    ReactDOM.render(<App />, rootElement);
+    render();
   });
 
-  // // Chunked polyfill for browsers without Intl support
-  // if (!window.Intl) {
-  //   (new Promise((resolve) => {
-  //     resolve(import('intl'));
-  //   }))
-  //     .then(() => Promise.all([
-  //       import('intl/locale-data/jsonp/en.js'),
-  //       import('intl/locale-data/jsonp/zh.js'),
-  //     ]))
-  //     .then(() => render(translationMessages))
-  //     .catch((err) => {
-  //       throw err;
-  //     });
-  // } else {
-  //   render(translationMessages);
-  // }
+  // Chunked polyfill for browsers without Intl support
+  if (!window.Intl) {
+    (new Promise((resolve) => {
+      resolve(import('intl'));
+    }))
+      .then(() => Promise.all([
+        import('intl/locale-data/jsonp/en.js'),
+        import('intl/locale-data/jsonp/zh.js'),
+      ]))
+      .then(() => render(translationMessages))
+      .catch((err) => {
+        throw err;
+      });
+  } else {
+    render(translationMessages);
+  }
 }
