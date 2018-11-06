9e4ef46c2d50baf1a58cf2016e7ae9b3f6024b6f
ylq <458909861@qq.com>
2018-11-06 11:53:12 +0800

    去掉无用代码

--- src/index.js
+++ src/index.js
@@ -3,23 +3,16 @@ import 'babel-polyfill';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { IntlProvider, addLocaleData } from 'react-intl';
+import { IntlProvider } from 'react-intl';
 
 import Main from 'containers/Main';
-import intl from 'intl';
-import zh from 'react-intl/locale-data/zh'; // react-intl语言包
-import en from 'react-intl/locale-data/en'; // react-intl语言包
 import { translationMessages } from './i18n';
-
-import zhTranslationMessages from './translations/zh';
-import enTranslationMessages from './translations/en';
 
 import storeFactory from './state/storeFactory';
 
 import './global-styles';
 import './theme/style.less';
 
-addLocaleData([...en, ...zh]);
 const rootElement = document.getElementById('root');
 const initialState = {};
 const store = storeFactory(initialState);
@@ -27,14 +20,11 @@ const store = storeFactory(initialState);
 function chooseLocale() {
   switch (navigator.language.split('-')[0]) {
     case 'en':
-      return enTranslationMessages;
-      break;
+      return translationMessages.en;
     case 'zh':
-      return zhTranslationMessages;
-      break;
+      return translationMessages.zn;
     default:
-      return enTranslationMessages;
-      break;
+      return translationMessages.en;
   }
 }
 
