7f00cc90d8694541ca704d577cc1b98bf236881b
lichun <chunli@sf-express.com>
2018-11-08 14:09:52 +0800

    fix babel preset

--- scripts/start.js
+++ scripts/start.js
@@ -43,6 +43,9 @@ app.get('*', (req, res) => {
   });
 });
 
-app.listen(8080, '127.0.0.1', () => {
+const customHost = process.env.HOST;
+const host = customHost || null; // Let http.Server use its default IPv6/4 host
+
+app.listen(8080, host, () => {
   console.log('Starting server on http://localhost:8080');
 });
