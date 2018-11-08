c12183ac9e05f5ec9869d0869bf5465177d582a9
lichun <chunli@sf-express.com>
2018-11-08 20:32:39 +0800

    for print

--- src/containers/Print/modules/PrintPage.js
+++ src/containers/Print/modules/PrintPage.js
@@ -14,6 +14,23 @@ import { NAMESPACE } from '../constants';
 import { getDataList, updateEntityModal, updateSearchCondition } from '../actions';
 import { selectSearchCondition } from '../selectors';
 
+const PrintArea = styled.div`
+  background: #fff;
+  width: 800px;
+  border 1px solid #999;
+
+  h1 {
+    text-align: center;
+  }
+  table {
+    width: 100%;
+    border-collapse: collapse;
+    td {
+      border: 1px solid #000;
+    }
+  }
+`;
+
 const withConnect = connectFactory(NAMESPACE);
 
 @withConnect(
@@ -38,18 +55,21 @@ class PrintPage extends React.Component {
     return (
       <div>
         <Button onClick={this.handlePrint}>打印</Button>
-        <div id="section-to-print">
-          <h1>我是标题</h1>
+        <PrintArea id="section-to-print">
+          <h1>入库验收单</h1>
           <table>
-            <tr>
-              <td>姓名</td>
-              <td>年龄</td>
-              <td>货号</td>
-              <td>出生日期</td>
-              <td>乱七八糟</td>
-            </tr>
+            <thead>
+              <tr>
+                <td>姓名</td>
+                <td>年龄</td>
+                <td>货号</td>
+                <td>出生日期</td>
+                <td>乱七八糟</td>
+              </tr>
+            </thead>
+            <tbody></tbody>
           </table>
-        </div>
+        </PrintArea>
       </div>);
   }
 }
