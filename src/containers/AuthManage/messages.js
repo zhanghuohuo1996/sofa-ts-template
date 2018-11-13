9d029723446fac65e1ab4ef45a0970cb0db73251
ylq <458909861@qq.com>
2018-11-13 16:48:21 +0800

    modify message

--- src/containers/AuthManage/messages.js
+++ src/containers/AuthManage/messages.js
@@ -37,61 +37,9 @@ export default defineMessages({
         defaultMessage: '无效',
       },
     },
-    createEntity: {
-      id: 'sofa.containers.authManage.createEntity',
-      defaultMessage: '创建实体',
-    },
-    habitualResidence: {
-      id: 'sofa.containers.authManage.habitualResidence',
-      defaultMessage: '常住地址',
-    },
-    website: {
-      id: 'sofa.containers.authManage.website',
-      defaultMessage: '网站',
-    },
-    basicModal: {
-      id: 'sofa.containers.authManage.basicModal',
-      defaultMessage: '基础 Modal',
-    },
-    nickname: {
-      id: 'sofa.containers.authManage.nickname',
-      defaultMessage: '昵称',
-    },
-    explainNickname: {
-      id: 'sofa.containers.authManage.explainNickname',
-      defaultMessage: '想让大家如何称呼你？',
-    },
-    createUser: {
-      id: 'sofa.containers.authManage.createUser',
-      defaultMessage: '创建用户',
-    },
-    editUser: {
-      id: 'sofa.containers.authManage.editUser',
-      defaultMessage: '修改用户',
-    },
-    dataAuth: {
-      id: 'sofa.containers.authManage.dataAuth',
-      defaultMessage: '数据权限',
-    },
-    operationAuth: {
-      id: 'sofa.containers.authManage.operationAuth',
-      defaultMessage: '操作权限',
-    },
     modifyInfo: {
       id: 'sofa.containers.authManage.modifyInfo',
       defaultMessage: '修改信息',
-    },
-    resetPassword: {
-      id: 'sofa.containers.authManage.resetPassword',
-      defaultMessage: '重置密码',
-    },
-    newPassword: {
-      id: 'sofa.containers.authManage.newPassword',
-      defaultMessage: '新密码',
-    },
-    confirmNewPassword: {
-      id: 'sofa.containers.authManage.confirmNewPassword',
-      defaultMessage: '确认新密码',
     },
   },
 });
